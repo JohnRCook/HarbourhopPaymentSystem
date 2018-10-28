df6180ac0a7df7f0a74d8c720c4785d2129c232a
Yuliya Khadasevich <y.khadasevich@coolblue.nl>
2018-10-28 23:40:52 +0100

    implement draft flow for creating and updating payment

--- HarbourhopPaymentSystem/Settings.cs
+++ HarbourhopPaymentSystem/Settings.cs
@@ -4,5 +4,10 @@
     {
         public string DefaultConnection { get; set; }
         public string MollieApiKey { get; set; }
+        public bool TestMode { get; set; }
+        public string TotpApiSecretKey { get; set; }
+        public string PaymentReceiveDanceCampUrl { get; set; }
+        public string PaymentSuccessUrl { get; set; }
+        public string PaymentFailedUrl { get; set; }
     }
 }
